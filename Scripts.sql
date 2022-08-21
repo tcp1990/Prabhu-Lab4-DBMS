30174364118a28f880c5536be0edfe018c1d201c
prabhu <prabhu_tc@yahoo.com>
2022-08-21 10:30:09 +0530

    [Adding Scripts.sql]

---
+++ Scripts.sql
@@ -0,0 +1,162 @@
+/*Creating DB*/
+CREATE DATABASE IF NOT EXISTS `order-directory`;
+
+/*Using DB*/
+USE `order-directory`;
+
+/*Creating supplier table*/
+CREATE TABLE IF NOT EXISTS supplier(
+SUPP_ID int primary key,
+SUPP_NAME varchar(50) NOT NULL,
+SUPP_CITY varchar(50),
+SUPP_PHONE varchar(10) NOT NULL
+);
+
+/*Creating customer table*/
+CREATE TABLE IF NOT EXISTS customer(
+CUS_ID INT NOT NULL,
+CUS_NAME VARCHAR(20) NOT NULL,
+CUS_PHONE VARCHAR(10) NOT NULL,
+CUS_CITY varchar(30) NOT NULL,
+CUS_GENDER CHAR,
+PRIMARY KEY (CUS_ID));
+
+/*Creating category table*/
+CREATE TABLE IF NOT EXISTS category (
+CAT_ID INT NOT NULL,
+CAT_NAME VARCHAR(20) NOT NULL,
+PRIMARY KEY (CAT_ID)
+);
+
+/*Creating product table*/
+CREATE TABLE IF NOT EXISTS product (
+PRO_ID INT NOT NULL,
+PRO_NAME VARCHAR(20) NOT NULL DEFAULT "Dummy",
+PRO_DESC VARCHAR(60),
+CAT_ID INT NOT NULL,
+PRIMARY KEY (PRO_ID),
+FOREIGN KEY (CAT_ID) REFERENCES CATEGORY (CAT_ID)
+);
+
+/*Creating supplier_pricing table*/
+CREATE TABLE IF NOT EXISTS supplier_pricing (
+PRICING_ID INT NOT NULL,
+PRO_ID INT NOT NULL,
+SUPP_ID INT NOT NULL,
+SUPP_PRICE INT DEFAULT 0,
+PRIMARY KEY (PRICING_ID),
+FOREIGN KEY (PRO_ID) REFERENCES PRODUCT (PRO_ID),
+FOREIGN KEY (SUPP_ID) REFERENCES SUPPLIER(SUPP_ID)
+);
+
+/*Creating order table*/
+CREATE TABLE IF NOT EXISTS `order` (
+ORD_ID INT NOT NULL,
+ORD_AMOUNT INT NOT NULL,
+ORD_DATE DATE,
+CUS_ID INT NOT NULL,
+PRICING_ID INT NOT NULL,
+PRIMARY KEY (ORD_ID),
+FOREIGN KEY (CUS_ID) REFERENCES CUSTOMER(CUS_ID),
+FOREIGN KEY (PRICING_ID) REFERENCES SUPPLIER_PRICING(PRICING_ID)
+);
+
+/*Creating rating table*/
+CREATE TABLE IF NOT EXISTS rating (
+RAT_ID INT NOT NULL,
+ORD_ID INT NOT NULL,
+RAT_RATSTARS INT NOT NULL,
+PRIMARY KEY (RAT_ID),
+FOREIGN KEY (ORD_ID) REFERENCES `order`(ORD_ID)
+);
+
+/*Inserting into supplier table*/
+INSERT INTO supplier VALUES(1,"Rajesh Retails","Delhi",'1234567890');
+INSERT INTO supplier VALUES(2,"Appario Ltd.","Mumbai",'2589631470');
+INSERT INTO supplier VALUES(3,"Knome products","Banglore",'9785462315');
+INSERT INTO supplier VALUES(4,"Bansal Retails","Kochi",'8975463285');
+INSERT INTO supplier VALUES(5,"Mittal Ltd.","Lucknow",'7898456532');
+
+/*Inserting into customer table*/
+INSERT INTO customer VALUES(1,"AAKASH",'9999999999',"DELHI",'M');
+INSERT INTO customer VALUES(2,"AMAN",'9785463215',"NOIDA",'M');
+INSERT INTO customer VALUES(3,"NEHA",'9999999999',"MUMBAI",'F');
+INSERT INTO customer VALUES(4,"MEGHA",'9994562399',"KOLKATA",'F');
+INSERT INTO customer VALUES(5,"PULKIT",'7895999999',"LUCKNOW",'M');
+
+/*Inserting into category table*/
+INSERT INTO category VALUES( 1,"BOOKS");
+INSERT INTO category VALUES(2,"GAMES");
+INSERT INTO category VALUES(3,"GROCERIES");
+INSERT INTO category VALUES (4,"ELECTRONICS");
+INSERT INTO category VALUES(5,"CLOTHES");
+
+/*Inserting into product table*/
+INSERT INTO product VALUES(1,"GTA V","Windows 7 and above with i5 processor and 8GB RAM",2);
+INSERT INTO product VALUES(2,"TSHIRT","SIZE-L with Black, Blue and White variations",5);
+INSERT INTO product VALUES(3,"ROG LAPTOP","Windows 10 with 15inch screen, i7 processor, 1TB SSD",4);
+INSERT INTO product VALUES(4,"OATS","Highly Nutritious from Nestle",3);
+INSERT INTO product VALUES(5,"HARRY POTTER","Best Collection of all time by J.K Rowling",1);
+INSERT INTO product VALUES(6,"MILK","1L Toned MIlk",3);
+INSERT INTO product VALUES(7,"Boat EarPhones","1.5Meter long Dolby Atmos",4);
+INSERT INTO product VALUES(8,"Jeans","Stretchable Denim Jeans with various sizes and color",5);
+INSERT INTO product VALUES(9,"Project IGI","compatible with windows 7 and above",2);
+INSERT INTO product VALUES(10,"Hoodie","Black GUCCI for 13 yrs and above",5);
+INSERT INTO product VALUES(11,"Rich Dad Poor Dad","Written by RObert Kiyosaki",1);
+INSERT INTO product VALUES(12,"Train Your Brain","By Shireen Stephen",1);
+
+/*Inserting into supplier_pricing table*/
+INSERT INTO supplier_pricing VALUES(1,1,2,1500);
+INSERT INTO supplier_pricing VALUES(2,3,5,30000);
+INSERT INTO supplier_pricing VALUES(3,5,1,3000);
+INSERT INTO supplier_pricing VALUES(4,2,3,2500);
+INSERT INTO supplier_pricing VALUES(5,4,1,1000);
+INSERT INTO supplier_pricing VALUES(6,12,2,780);
+INSERT INTO supplier_pricing VALUES(7,12,4,789);
+INSERT INTO supplier_pricing VALUES(8,3,1,31000);
+INSERT INTO supplier_pricing VALUES(9,1,5,1450);
+INSERT INTO supplier_pricing VALUES(10,4,2,999);
+INSERT INTO supplier_pricing VALUES(11,7,3,549);
+INSERT INTO supplier_pricing VALUES(12,7,4,529);
+INSERT INTO supplier_pricing VALUES(13,6,2,105);
+INSERT INTO supplier_pricing VALUES(14,6,1,99);
+INSERT INTO supplier_pricing VALUES(15,2,5,2999);
+INSERT INTO supplier_pricing VALUES(16,5,2,2999);
+
+/*Inserting into order table*/
+INSERT INTO `order` VALUES (101,1500,"2021-10-06",2,1);
+INSERT INTO `order` VALUES(102,1000,"2021-10-12",3,5);
+INSERT INTO `order` VALUES(103,30000,"2021-09-16",5,2);
+INSERT INTO `order` VALUES(104,1500,"2021-10-05",1,1);
+INSERT INTO `order` VALUES(105,3000,"2021-08-16",4,3);
+INSERT INTO `order` VALUES(106,1450,"2021-08-18",1,9);
+INSERT INTO `order` VALUES(107,789,"2021-09-01",3,7);
+INSERT INTO `order` VALUES(108,780,"2021-09-07",5,6);
+INSERT INTO `order` VALUES(109,3000,"2021-09-10",5,3);
+INSERT INTO `order` VALUES(110,2500,"2021-09-10",2,4);
+INSERT INTO `order` VALUES(111,1000,"2021-09-15",4,5);
+INSERT INTO `order` VALUES(112,789,"2021-09-16",4,7);
+INSERT INTO `order` VALUES(113,31000,"2021-09-16",1,8);
+INSERT INTO `order` VALUES(114,1000,"2021-09-16",3,5);
+INSERT INTO `order` VALUES(115,3000,"2021-09-16",5,3);
+INSERT INTO `order` VALUES(116,99,"2021-09-17",2,14);
+
+/*Inserting into rating table*/
+INSERT INTO rating VALUES(1,101,4);
+INSERT INTO rating VALUES(2,102,3);
+INSERT INTO rating VALUES(3,103,1);
+INSERT INTO rating VALUES(4,104,2);
+INSERT INTO rating VALUES(5,105,4);
+INSERT INTO rating VALUES(6,106,3);
+INSERT INTO rating VALUES(7,107,4);
+INSERT INTO rating VALUES(8,108,4);
+INSERT INTO rating VALUES(9,109,3);
+INSERT INTO rating VALUES(10,110,5);
+INSERT INTO rating VALUES(11,111,3);
+INSERT INTO rating VALUES(12,112,4);
+INSERT INTO rating VALUES(13,113,2);
+INSERT INTO rating VALUES(14,114,1);
+INSERT INTO rating VALUES(15,115,1);
+INSERT INTO rating VALUES(16,116,0);
+
+/*3)	Display the total number of customers based on gender who have placed orders of worth at least Rs.3000*/
